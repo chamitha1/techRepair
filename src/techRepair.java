6ef8f6174a15c8457ec45db5e657005c28e4cf9a
Chamitha <chamithashelan@gmail.com>
2024-08-25 13:59:35 +0530

    Created main form

---
+++ src/techRepair.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class techRepair {
+
+    private JPanel mainPanel;
+    private JButton manageCustomerOrdersButton;
+    private JButton manageSuppliersButton;
+    private JButton manageInventoryButton;
+    private JButton manageEmployeesButton;
+    private JButton generateReportButton;
+
+    public techRepair() {
+        manageCustomerOrdersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
