b9c20d31e6364dcfa8d6a87a95b3fd002920abb3
Chamitha <chamithashelan@gmail.com>
2024-08-26 07:44:58 +0530

    Set up the classes and forms

---
+++ src/generateMonthlyReportsForm.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class generateMonthlyReportsForm extends JFrame {
+
+}
--- src/techRepair.java
+++ src/techRepair.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class techRepair {
+public class techRepair extends JFrame {
 
     private JPanel mainPanel;
     private JButton manageCustomerOrdersButton;
@@ -15,12 +15,42 @@ public class techRepair {
         manageCustomerOrdersButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                new manageCustomerOrdersForm();
+            }
+        });
+        manageSuppliersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new manageSuppliersForm();
+            }
+        });
+        manageInventoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new manageInventoryForm();
+            }
+        });
+        manageEmployeesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new manageEmployeesForm();
+            }
+        });
+        generateReportButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new generateMonthlyReportsForm();
             }
         });
     }
 
     public static void main(String[] args) {
+        techRepair mainWindow = new techRepair();
+        mainWindow.setTitle("Tech Repair");
+        mainWindow.setContentPane(mainWindow.mainPanel);
+        mainWindow.setSize(300,400);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setVisible(true);
 
     }
 
